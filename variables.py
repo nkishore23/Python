eab898ff030d2ef4a05f58ef801e9498beab7cd5
Kishore N <nkishore2390@gmail.com>
2025-12-10 15:45:55 +0530

    first commit

---
+++ variables.py
@@ -0,0 +1,14 @@
+name = "Kishore"
+age = 25
+country = "India"
+
+#Normal String Output
+
+
+print("Name:", name);
+print("Age:", age);
+print("Country:", country);
+
+
+# Formatted String Output
+print(f"My name is {name}, I am {age} years old and I live in {country}.")
